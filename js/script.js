e112d96c793ebb87e6b5a854dad61b45352fdd96
cdownie1 <cjdownie90@gmail.com>
2021-06-14 14:00:56 +0100

    form styling added

--- js/script.js
+++ js/script.js
@@ -2,10 +2,12 @@
 const header = document.querySelector('.header');
 const aboutSection = document.querySelector('#about');
 const projectSection = document.querySelector('#projects');
-// const contactSection = document.querySelector('#contact');
+const contactSection = document.querySelector('#contact');
 
 const projectBtn = document.querySelector('.btn-header__project');
+// const contactBtn = document.querySelector('.btn-header__contact');
 const arrowBtn = document.querySelector('.scroll');
+const formLabels = document.querySelectorAll('.contact label');
 
 //smooth scroll on section buttons
 projectBtn.addEventListener('click', function (e) {
@@ -15,9 +17,26 @@ projectBtn.addEventListener('click', function (e) {
     })
 })
 
+// projectBtn.addEventListener('click', function (e) {
+//     e.preventDefault()
+//     contactSection.scrollIntoView({
+//         behavior: 'smooth'
+//     })
+// })
+
 arrowBtn.addEventListener('click', function (e) {
     e.preventDefault()
     aboutSection.scrollIntoView({
         behavior: 'smooth'
     })
+})
+
+
+//form label animation
+
+formLabels.forEach(label => {
+    label.innerHTML = label.innerHTML
+        .split('')
+        .map((letter, index) => `<span style="transition-delay:${index * 50}ms">${letter}</span>`)
+        .join('');
 })

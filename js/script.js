ee39337928ffe6e3e8cbf1c22a118fdda1581cfc
cdownie1 <cjdownie90@gmail.com>
2021-06-15 13:46:45 +0100

    finalised nav

--- js/script.js
+++ js/script.js
@@ -5,38 +5,52 @@ const projectSection = document.querySelector('#projects');
 const contactSection = document.querySelector('#contact');
 
 const projectBtn = document.querySelector('.btn-header__project');
-// const contactBtn = document.querySelector('.btn-header__contact');
 const arrowBtn = document.querySelector('.scroll');
 const formLabels = document.querySelectorAll('.contact label');
 
+const homeBtn = document.querySelector('.nav__main-link-home');
+const aboutBtn = document.querySelector('.nav__main-link-about');
+const projectsBtn = document.querySelector('.nav__main-link-projects');
+const contactBtn = document.querySelector('.nav__main-link-contact');
+
+const navCheckbox = document.querySelector('.nav__checkbox');
+
+//nav smooth scrolling
+document
+  .querySelector('.nav__main-list')
+  .addEventListener('click', function (ev) {
+    ev.preventDefault();
+
+    const id = ev.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+    });
+    navCheckbox.checked = false;
+  });
+
 //smooth scroll on section buttons
 projectBtn.addEventListener('click', function (e) {
-    e.preventDefault()
-    projectSection.scrollIntoView({
-        behavior: 'smooth'
-    })
-})
-
-// projectBtn.addEventListener('click', function (e) {
-//     e.preventDefault()
-//     contactSection.scrollIntoView({
-//         behavior: 'smooth'
-//     })
-// })
+  e.preventDefault();
+  projectSection.scrollIntoView({
+    behavior: 'smooth',
+  });
+});
 
 arrowBtn.addEventListener('click', function (e) {
-    e.preventDefault()
-    aboutSection.scrollIntoView({
-        behavior: 'smooth'
-    })
-})
-
+  e.preventDefault();
+  aboutSection.scrollIntoView({
+    behavior: 'smooth',
+  });
+});
 
 //form label animation
 
-formLabels.forEach(label => {
-    label.innerHTML = label.innerHTML
-        .split('')
-        .map((letter, index) => `<span style="transition-delay:${index * 50}ms">${letter}</span>`)
-        .join('');
-})
+formLabels.forEach((label) => {
+  label.innerHTML = label.innerHTML
+    .split('')
+    .map(
+      (letter, index) =>
+        `<span style="transition-delay:${index * 50}ms">${letter}</span>`
+    )
+    .join('');
+});
